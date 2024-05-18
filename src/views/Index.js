dd7ead92b669a95ce6045facf350bd571da25204
pchawin40 <pathocha000@gmail.com>
2024-05-17 18:58:13 -0700

    Moved git to root of directory | 6:58 PM 5.17.24

---
+++ src/views/Index.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import IndexHeader from "components/Headers/IndexHeader.js";
+import DemoFooter from "components/Footers/DemoFooter.js";
+
+// index sections
+import SectionCarousel from "views/index-sections/SectionCarousel.js";
+import SectionDark from "views/index-sections/SectionDark.js";
+
+function Index() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("index");
+    return function cleanup() {
+      document.body.classList.remove("index");
+    };
+  });
+  return (
+    <>
+      <IndexNavbar />
+      <IndexHeader />
+      <div className="main">
+        <SectionCarousel />
+        <SectionDark />
+        <DemoFooter />
+      </div>
+    </>
+  );
+}
+
+export default Index;
---
+++ src/views/index-sections/SectionDark.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// core components
+
+function SectionDark() {
+  return (
+    <>
+      <div className="section section-dark">
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto text-center" md="8">
+              <h2 className="title">TOKEN ADDRESS</h2>
+              <h4 className="description">
+                TEXTHOLDER129329
+              </h4>
+              <h4/>
+              <p className="description">
+              In the hushed hours of the night, a friendly ghost haunts the cozy kitchen, eagerly preparing for a spectral feast. As it delicately places slices of bread into the glowing toaster, the air fills with the comforting scent of toasting bread. This ghostly culinary artist invites you to join in the delight of its favorite midnight snack.
+
+              Come, indulge in the warmth of golden, buttery toast with our cheerful ghost. Each bite is not just food, but a story of comfort and joy shared in the moonlit kitchen. Let the "Ghost with Toast" charm your nights with its simple, heartwarming magic.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default SectionDark;
